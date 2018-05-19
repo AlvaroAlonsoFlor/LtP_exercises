0defba3889530c277a3195bcbbd9e04e3b3e5fb5
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-05-19 12:59:30 +0100

    Start orange tree exercise

---
+++ orange_tree.rb
@@ -0,0 +1,20 @@
+=begin
+Make an OrangeTree class that has a height method that returns it's height
+After a number of years the tree must die
+During the first few years the tree should not produce fruit
+The production should be increasing with the years (and decreasing at the end?)
+Be able to count the oranges and pick one
+=end
+
+class OrangeTree
+  def initialize
+  end
+
+  #method that returns its height
+
+  #A one_year_passes method
+
+  #A count oranges method
+
+  #A pick an orange method
+end
