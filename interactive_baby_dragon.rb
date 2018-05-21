1d31ea5caf0ad36b7c88b89d73498b4dbcff4cec
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-05-21 18:53:14 +0100

    Add sketch of interactive baby dragon

---
+++ interactive_baby_dragon.rb
@@ -0,0 +1,29 @@
+=begin
+Write a program that lets you enter commands such as feed and walk.
+Since we are putting just string we need a dispatch method that checks
+which string was entered and then calls the appropiate method
+=end
+
+class BabyDragon
+  def initialize
+    @name = name
+  end
+
+  def feed #A function for feeding
+  end
+
+  def walk #A function for walking
+  end
+
+  def sleep
+  end
+
+  def time_passes
+  end
+
+  def command #A function to check the comands
+  end
+
+  def dispatch #Checks the strings
+  end
+end
